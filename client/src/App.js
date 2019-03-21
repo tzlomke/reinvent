c0d3bc21311e00caf7487ccf6b922d4570f59c41
Chieftjz <taylor.zlomke@gmail.com>
2019-03-20 22:41:05 -0600

    created UserProfile component folder and files

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import CampaignDisplay from "./components/CampaignDisplay";
+import CampaignForm from "./components/CampaignForm";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <CampaignDisplay />
+        <CampaignForm />
       </div>
     );
   }
