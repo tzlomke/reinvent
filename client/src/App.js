5766e72f3b99fb896be8937a4c6f53203c5e4226
Brant Keener <brant_keener@yahoo.com>
2019-03-22 21:51:32 -0600

    remove ideas and ideasdiscussed from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import Ideas from './pages/Ideas';
-import IdeasDiscussed from "./pages/IdeasDiscussed";
+// import Ideas from './pages/Ideas';
+// import IdeasDiscussed from "./pages/IdeasDiscussed";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Ideas />
-        <IdeasDiscussed />
       </div>
     );
   }
