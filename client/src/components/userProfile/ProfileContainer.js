1d2a2f6dfe1f9ca72e847c4f7d071a75ca9798dc
Chieftjz <taylor.zlomke@gmail.com>
2019-03-22 19:39:17 -0600

    Profile Scaffold

---
+++ client/src/components/userProfile/ProfileContainer.js
@@ -0,0 +1,3 @@
+import React, { Component } from "react";
+import ProfilePicture from "./ProfilePicture";
+import ProfileData from "./ProfileData";
---
+++ client/src/components/userProfile/ProfilePicture.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+
+class ProfilePicture extends Component {
+
+	state = {
+		hasImage: false,
+		imageFile: {}
+	};
+
+	// Needs: Upload and Render Image File
+	// Find A Place to Host Images
+	// ? Stateful or Stateless?
+
+	render() {
+		<div></div>
+	};
+};
+
+export default ProfilePicture;
