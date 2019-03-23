35f67368308f26b2ae870c73227eb84e66ce463c
Chieftjz <taylor.zlomke@gmail.com>
2019-03-22 21:19:17 -0600

    created stateless profile data component

--- client/src/components/userProfile/ProfileData.js
+++ client/src/components/userProfile/ProfileData.js
@@ -1,23 +1,49 @@
 import React, { Component } from "react";
-import API from "../../utils/API";
+// import API from "../../utils/API";
 
-class ProfileData extends Component {
-
-	state = {
-		userInformation: {},
-		userBio: "",
-		userCampaigns: {},
-		userVotes: {}
-	};
+const ProfileData = (props) => {
+	const { userFullName, username, userOwnedCampaings, userParticipatingCampaigns } = props;
 
 	// Needs: Render User Info, Render User Campaign Data as Links, (Other Campaigns User Participates In? Upload and Render Bio?)
 	// ? Stateful or Stateless?
 
-	render() {
-		return(
-			<div></div>
-		);
-	};
+
+	return(
+		<div>
+
+			<header>
+				<h1>{userFullName}</h1>
+				<h4>{username}</h4>
+			</header>
+
+			<section className="user-data">
+
+				<div className="user-campaigns">
+
+					<h3>My Campaigns</h3>
+					<ul className="campaign-list">
+						{userOwnedCampaings.map(campaign => (
+							<li className="campaign" key={campaign.id}>
+								{campaign.title}
+							</li>
+						))}		
+					</ul>
+					
+					<h3>Other Campaigns</h3>
+					<ul className="campaign-list">
+						{userParticipatingCampaigns.map(campaign => (
+							<li className="campaign" key={campaign.id}>
+								{campaign.title}
+							</li>
+						))}
+					</ul>
+					
+				</div>
+
+			</section>
+
+		</div>
+	);
 };
 
 export default ProfileData;
