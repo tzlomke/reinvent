087d22cb311d3cfff5e9542ec4ac232be91323ca
Taylor Zlomke <taylor.zlomke@gmail.com>
2019-03-24 21:09:00 -0600

    conditionally rendered profile page elements based on whether profile page belong's to logged in user

--- client/src/components/userProfile/ProfileData.js
+++ client/src/components/userProfile/ProfileData.js
@@ -1,32 +1,57 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
 
 const ProfileData = (props) => {
-	const { userFullName, username, userCampaigns } = props;
+	const { userFullName, username, userID, authenticatedUserID } = props;
 
 	// Needs: Render User Info, Render User Campaign Data as Links, (Other Campaigns User Participates In? Upload and Render Bio?)
 	// ? Stateful or Stateless?
-
-
-	return(
-		<div>
-			<header>
-				<h1>{userFullName}</h1>
-				<h4>{username}</h4>
-			</header>
-			<section className="user-data">
-				<div className="user-campaigns">
-					<h3>My Campaigns</h3>
-					{/* <ul className="campaign-list">
-						{userCampaigns.map(campaign => (
-							<li className="campaign" key={campaign.id}>
-								{campaign.title}
-							</li>
-						))}		
-					</ul> */}
-				</div>
-			</section>
-		</div>
-	);
+	if (props.userID === props.authenticatedUserID) {
+		return(
+			<div>
+				<header>
+					<h1>{userFullName}</h1>
+					<h4>{username}</h4>
+				</header>
+				<section className="user-data">
+					<div className="user-campaigns">
+						<h3>My Campaigns</h3>
+						{/* <ul className="campaign-list">
+							{userCampaigns.map(campaign => (
+								<li className="campaign" key={campaign.id}>
+									{campaign.title}
+								</li>
+							))}		
+						</ul> */}
+					</div>
+				</section>
+			</div>
+		);
+	} else {
+		return(
+			<div>
+				<header>
+					<h1>{userFullName}</h1>
+					<h4>{username}</h4>
+				</header>
+				<section className="user-data">
+					<div className="user-campaigns">
+						<h3>{userFullName}'s Campaigns</h3>
+						{/* <ul className="campaign-list">
+							{userCampaigns.map(campaign => (
+								<li className="campaign" key={campaign.id}>
+									{campaign.title}
+								</li>
+							))}		
+						</ul> */}
+					</div>
+				</section>
+			</div>
+		);
+	}
+	
 };
 
 export default ProfileData;
--- client/src/components/userProfile/index.js
+++ client/src/components/userProfile/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
 import ProfileData from "./ProfileData";
 import ProfilePicture from "./ProfilePicture";
+
 
 class UserProfile extends Component {
 
 	state = {
+		userID: "",
 		userFullName: "",
 		username: "",
 		userCampaigns: [],
@@ -22,11 +27,11 @@ class UserProfile extends Component {
 	loadUser = () => {
 		let userParam = this.props.location.pathname;
 		let username = userParam.split("/")[2];
-		console.log(username)
 		API.getUser(username)
 			.then(response => {
 				let userData = response.data[0]
 				this.setState({
+					userID: userData._id,
 					userFullName: `${userData.firstName} ${userData.lastName}`,
 					username: userData.username,
 					userCampaigns: userData.campaigns,
@@ -40,6 +45,9 @@ class UserProfile extends Component {
 	};
 
 	render() {
+		const { user } = this.props.auth;
+		console.log(user);
+
 		return(
 			<div className="profile-wrapper">
 				<ProfilePicture 
@@ -56,4 +64,13 @@ class UserProfile extends Component {
 	};
 }
 
-export default UserProfile;
+UserProfile.propTypes = {
+	logoutUser: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  	auth: state.auth
+});
+
+export default connect(mapStateToProps, { logoutUser })(UserProfile);
