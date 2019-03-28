31ab0dfb7845a4149bd2ce7f532caa11cda62d7c
Brant Keener <brant_keener@yahoo.com>
2019-03-27 21:33:47 -0600

    keep working on ideas/discussions

--- client/src/pages/ActiveVoteIdeas/index.js
+++ client/src/pages/ActiveVoteIdeas/index.js
@@ -8,6 +8,8 @@ class ActiveVoteIdeas extends Component {
   state = {
     campaignsFromDB: [],
     userId: "1",
+    campaignClicked: {},
+    campaignExpand: false
   }
 
   voteId="";
@@ -19,7 +21,7 @@ class ActiveVoteIdeas extends Component {
       .then(response => {
         campaignArray.push(response.data);
         this.setState({ campaignsFromDB: campaignArray });
-        console.log(response.data);
+        // console.log(response.data);
       });
   };
 
@@ -69,7 +71,17 @@ class ActiveVoteIdeas extends Component {
   handleData = (voteId, campaignId) => {
     this.voteId = voteId;
     this.campaignId = campaignId;
-    console.log(this.voteId, this.campaignId);
+    // console.log(this.voteId, this.campaignId);
+  };
+
+  campaignExpand = (campaignId) => {
+    // this.setState({ campaignClicked: { expand: true }});
+    API.campaignGet(campaignId)
+      .then(response => {
+        const resDat = response.data[0];
+        this.setState({ campaignClicked: resDat, campaignExpand: true });
+        console.log(resDat);
+      });
   };
 
   componentDidMount = () => {
@@ -77,55 +89,110 @@ class ActiveVoteIdeas extends Component {
   };
 
   render(){
+    const campaignsFromDB = this.state.campaignsFromDB;
+    const campaignClicked = this.state.campaignClicked;
+    console.log(this.state.campaignClicked);
+    console.log(this.state.campaignExpand)
     return (
-      <div>
-        {this.state.campaignsFromDB.map(campaign =>
-          campaign.map(campaign => (
-            campaign.vote.length  !== 0 ? (
-              console.log(campaign.vote[0]._id),
-              <CampaignDisplay
-              handleData={()=>this.handleData(campaign.vote[0]._id, campaign._id)}
-              data={campaign.vote}
-              title={campaign.title}
-              author={campaign.author}
-              synopsis={campaign.synopsis}
-              key={campaign._id}
-              styles={{opacity:1}}
-              // text={customText}
-              onCreate={this.onCreate}
-              onUpvote={this.onUpvote}
-              onClose={this.onClose}
-              onReset={this.onReset}
-              onDownvote={this.onDownvote}
-              onExpand={this.onExpand}
-              onEdit={this.onEdit}
-              isAdmin={true}
-              clientId={"1"}
-              />
-            ):(
-              <CampaignDisplay
-              handleData={()=>this.handleData(campaign.vote._id, campaign._id)}
-              data={campaign.vote}
-              title={campaign.title}
-              author={campaign.author}
-              synopsis={campaign.synopsis}
-              key={campaign._id}
-              styles={{opacity:1}}
-              // text={customText}
-              onCreate={this.onCreate}
-              onUpvote={this.onUpvote}
-              onClose={this.onClose}
-              onReset={this.onReset}
-              onDownvote={this.onDownvote}
-              onExpand={this.onExpand}
-              onEdit={this.onEdit}
-              isAdmin={true}
-              clientId={"1"}
-              />
-            ) 
-          ))
-        )}
-      </div>
+      !this.state.campaignExpand ? (
+        <div>
+          {campaignsFromDB.map(campaign =>
+            campaign.map(campaign => (
+              campaign.vote.length  !== 0 ? (
+                console.log(campaign.vote[0]._id),
+                <CampaignDisplay
+                handleData={()=>this.handleData(campaign.vote[0]._id, campaign._id)}
+                campaignExpand={() => this.campaignExpand(campaign._id)}
+                data={campaign.vote}
+                title={campaign.title}
+                author={campaign.author}
+                synopsis={campaign.synopsis}
+                key={campaign._id}
+                styles={{opacity:1}}
+                // text={customText}
+                onCreate={this.onCreate}
+                onUpvote={this.onUpvote}
+                onClose={this.onClose}
+                onReset={this.onReset}
+                onDownvote={this.onDownvote}
+                onExpand={this.onExpand}
+                onEdit={this.onEdit}
+                isAdmin={true}
+                clientId={"1"}
+                />
+              ):(
+                <CampaignDisplay
+                handleData={()=>this.handleData(campaign.vote._id, campaign._id)}
+                campaignExpand={() => this.campaignExpand(campaign._id)}
+                data={campaign.vote}
+                title={campaign.title}
+                author={campaign.author}
+                synopsis={campaign.synopsis}
+                key={campaign._id}
+                styles={{opacity:1}}
+                // text={customText}
+                onCreate={this.onCreate}
+                onUpvote={this.onUpvote}
+                onClose={this.onClose}
+                onReset={this.onReset}
+                onDownvote={this.onDownvote}
+                onExpand={this.onExpand}
+                onEdit={this.onEdit}
+                isAdmin={true}
+                clientId={"1"}
+                />
+              ) 
+            ))
+          )}
+        </div>
+      ) : (
+        <div>
+          {campaignClicked.vote[0].length  > 1 ? (
+            console.log('campaign it' + campaignClicked.vote[0]._id),
+            <CampaignDisplay
+            // handleData={()=>this.handleData(campaign.vote[0]._id, campaign._id)}
+            // campaignExpand={() => this.campaignExpand(campaign._id)}
+            data={campaignClicked.vote}
+            title={campaignClicked.title}
+            author={campaignClicked.author}
+            synopsis={campaignClicked.synopsis}
+            key={campaignClicked._id}
+            styles={{opacity:1}}
+            // text={customText}
+            onCreate={this.onCreate}
+            onUpvote={this.onUpvote}
+            onClose={this.onClose}
+            onReset={this.onReset}
+            onDownvote={this.onDownvote}
+            onExpand={this.onExpand}
+            onEdit={this.onEdit}
+            isAdmin={true}
+            clientId={"1"}
+            />
+          ):(
+            <CampaignDisplay
+            // handleData={()=>this.handleData(campaign.vote._id, campaign._id)}
+            // campaignExpand={() => this.campaignExpand(campaign._id)}
+            data={campaignClicked.vote}
+            title={campaignClicked.title}
+            author={campaignClicked.author}
+            synopsis={campaignClicked.synopsis}
+            key={campaignClicked._id}
+            styles={{opacity:1}}
+            // text={customText}
+            onCreate={this.onCreate}
+            onUpvote={this.onUpvote}
+            onClose={this.onClose}
+            onReset={this.onReset}
+            onDownvote={this.onDownvote}
+            onExpand={this.onExpand}
+            onEdit={this.onEdit}
+            isAdmin={true}
+            clientId={"1"}
+            />
+          )}
+        </div>
+      )
     )
   }
 }
