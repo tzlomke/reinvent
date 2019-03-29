98eedc3276c44c8f0a26b863c93f384d5d22852a
Taylor Zlomke <taylor.zlomke@gmail.com>
2019-03-28 18:45:47 -0600

    created profileImageUpload controller and route

--- routes/api/userData.js
+++ routes/api/userData.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const userDataController = require('../../controllers/userDataConroller');
 
+// Image Upload function in controller or directly on route?????????
+
 router.route("/:username")
 	.get(userDataController.getUser);
+
+router.route("/:username/profile-image")
+	.post(userDataController.uploadProfileImage);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const path = require("path");
 
 // Environmental Variables
-require('dotenv').config({path: path.join(__dirname, '.env')})
+require('dotenv').config({
+	path: path.join(__dirname, '.env')
+});
 
 const express = require("express");
 const mongoose = require("mongoose");
-
-// const db = require("./models");
 const passport = require("passport");
+const aws = require("aws-sdk");
+const multlerS3 = require("multer-s3");
+const multer = require("multer");
 
 // Port
 const PORT = process.env.PORT || 3001;
@@ -15,8 +18,8 @@ const PORT = process.env.PORT || 3001;
 // Initialize Express
 const app = express();
 
+// Routes
 const routes = require("./routes");
-
 
 // Passport Middleware
 app.use(passport.initialize());
@@ -26,30 +29,31 @@ require("./config/passport")(passport);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+	app.use(express.static("client/build"));
 };
 
 // Define middleware here
 app.use(express.urlencoded({
-  extended: true
+	extended: true
 }));
 app.use(express.json());
 
-// Add routes, both API and view
+// Use Routes
 app.use(routes);
 
 // Connect to DataBase
 mongoose
-	.connect(process.env.MONGODB_URI || "mongodb://localhost/reniventdb", { useNewUrlParser: true })
+	.connect(process.env.MONGODB_URI || "mongodb://localhost/reniventdb", {
+		useNewUrlParser: true
+	})
 	.then(console.log("Connected to MongoDB!"));
-
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
 	res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
 	console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
