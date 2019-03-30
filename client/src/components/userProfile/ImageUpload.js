066888e058a58b3a33d7f503d528cb83c482bceb
Taylor Zlomke <taylor.zlomke@gmail.com>
2019-03-30 11:37:42 -0600

    updated image upload path, hardcode bug removed

--- client/src/components/userProfile/ImageUpload.js
+++ client/src/components/userProfile/ImageUpload.js
@@ -1,34 +1,52 @@
 import React, { Component } from 'react';
+import API from "../../utils/API";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
 import axios from 'axios';
 import $ from 'jquery';
-class Home extends Component {
+
+class ImageUpload extends Component {
+
 	constructor( props ) {
 		super( props );
 		this.state = {
 			selectedFile: null,
-			selectedFiles: null
+			selectedFiles: null,
+			username: ""
 		}
 	};
 
-	singleFileChangedHandler = ( event ) => {
+	singleFileChangedHandler = (event) => {
 		this.setState({
 			selectedFile: event.target.files[0]
 		});
 	};
+
+	loadUser = () => {
+		let authenticatedUserId = this.props.auth.user.id
+			API.getUserById(authenticatedUserId)
+				.then(response => {
+					let userData = response.data[0]
+					this.setState({
+						username: userData.username,
+					});
+				});
+	};
 	
-	singleFileUploadHandler = (  ) => {
+	singleFileUploadHandler = () => {
 		const data = new FormData();
 		// If file selected
 		if ( this.state.selectedFile ) {
-			data.append( 'profileImage', this.state.selectedFile, this.state.selectedFile.name );
-			axios.post( '/api/profile/tzlomke/profile-image-upload', data, {
+			data.append('profileImage', this.state.selectedFile, this.state.selectedFile.name);
+			axios.post(`/api/profile/${this.state.username}/profile-image-upload`, data, {
 				headers: {
-				'accept': 'application/json',
-				'Accept-Language': 'en-US,en;q=0.8',
-				'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+					'accept': 'application/json',
+					'Accept-Language': 'en-US,en;q=0.8',
+					'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
 				}
 			})
-			.then( ( response ) => {
+			.then((response) => {
 				if ( 200 === response.status ) {
 					// If file size is larger than expected.
 					if( response.data.error ) {
@@ -56,44 +74,58 @@ class Home extends Component {
 		}
 	};
 
-// ShowAlert Function
- ocShowAlert = ( message, background = '#3089cf' ) => {
-  let alertContainer = document.querySelector( '#oc-alert-container' ),
-   alertEl = document.createElement( 'div' ),
-   textNode = document.createTextNode( message );
-  alertEl.setAttribute( 'class', 'oc-alert-pop-up' );
-  $( alertEl ).css( 'background', background );
-  alertEl.appendChild( textNode );
-  alertContainer.appendChild( alertEl );
-  setTimeout( function () {
-   $( alertEl ).fadeOut( 'slow' );
-   $( alertEl ).remove();
-  }, 3000 );
- };
+	// ShowAlert Function
+	ocShowAlert = (message, background = '#3089cf') => {
+		let alertContainer = document.querySelector('#oc-alert-container'),
+		alertEl = document.createElement('div'),
+		textNode = document.createTextNode(message);
+		alertEl.setAttribute('class', 'oc-alert-pop-up');
+		$(alertEl).css('background', background);
+		alertEl.appendChild(textNode);
+		alertContainer.appendChild(alertEl);
+		setTimeout(function () {
+			$(alertEl).fadeOut('slow');
+			$(alertEl).remove();
+		}, 3000);
+	};
 
-render() {
-  return(
-   <div>
-    <div className="container">
-     {/* For Alert box*/}
-     <div id="oc-alert-container"></div>
-{/* Single File Upload*/}
-     <div className="card border-light mb-3 mt-5" style={{ boxShadow: '0 5px 10px 2px rgba(195,192,192,.5)' }}>
-      <div className="card-header">
-       <h3 style={{ color: '#555', marginLeft: '12px' }}>Image Upload</h3>
-       <p className="text-muted" style={{ marginLeft: '12px' }}>Upload Size: 250px x 250px ( Max 2MB )</p>
-      </div>
-      <div className="card-body">
-       <p className="card-text">Please upload an image for your profile</p>
-       <input type="file" onChange={this.singleFileChangedHandler}/>
-       <div className="mt-5">
-        <button className="btn btn-info" onClick={this.singleFileUploadHandler}>Upload!</button>
-       </div>
-      </div>
-     </div>
-</div>
-   </div>
-  );
- }
+	componentDidMount =() => {
+		this.loadUser()
+	};
+
+	render() {
+		return(
+			<div>
+				<div className="container">
+					{/* For Alert box*/}
+					<div id="oc-alert-container"></div>
+					{/* Single File Upload*/}
+					<div className="card border-light mb-3 mt-5" style={{ boxShadow: '0 5px 10px 2px rgba(195,192,192,.5)' }}>
+						<div className="card-header">
+							<h3 style={{ color: '#555', marginLeft: '12px' }}>Image Upload</h3>
+							<p className="text-muted" style={{ marginLeft: '12px' }}>Upload Size: 250px x 250px ( Max 2MB )</p>
+						</div>
+						<div className="card-body">
+							<p className="card-text">Please upload an image for your profile</p>
+							<input type="file" onChange={this.singleFileChangedHandler}/>
+							<div className="mt-5">
+								<button className="btn btn-info" onClick={this.singleFileUploadHandler}>Upload!</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
-export default Home;
+
+ImageUpload.propTypes = {
+	logoutUser: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  	auth: state.auth
+});
+
+export default connect(mapStateToProps, { logoutUser })(ImageUpload);
