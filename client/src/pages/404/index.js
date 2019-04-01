0b27d4e173a9af30482aad8c085b0af0899b8b03
Brant Keener <brant_keener@yahoo.com>
2019-04-01 11:29:07 -0600

    Add 404 page

--- client/src/pages/404/index.js
+++ client/src/pages/404/index.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import donotpass from '../../images/none_shall_pass.jpg';
+import './style.css';
+import Header from '../../components/Header';
 
 const page404 = () => {
   return(
     <div>
-      <p>You can't go here!!</p>
+      <Header />
+      <div id="container404">
+      <a id="return_link" href="/dashboard">Take Me Home</a>
+        <img id="dead_link" src={donotpass} alt="Dead Link"></img>
+      </div>
     </div>
   )
 }
