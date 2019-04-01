31f00e08198384951e96800320e7b7e6089470f4
Taylor Zlomke <taylor.zlomke@gmail.com>
2019-04-01 00:31:23 -0600

    added parallax scroll to landing page

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,7 +1,7 @@
 import React from "react";
-import lightbulbCutout from "../../images/lightbulbCutout.png";
+import SpaceGirl from "../../images/Retro-Astronaut2.png"
 import "./style.css";
-
+import { Parallax } from "react-parallax";
 
 
 const Landing = (props) => {
@@ -9,61 +9,65 @@ const Landing = (props) => {
 	return (
 		
 		<div className="landing-page-container">
+			<Parallax
+				bgImage={SpaceGirl}
+				bgImageAlt="Girl in Space Helmet"
+				strength={300}
+			>
+				{/* Splash Page */}
+				<div className="background-container">
+					{/* Animated Background */}
+					<div>
 
-			{/* Splash Page */}
-			<div className="background-container">
-				{/* Animated Background */}
-				<div className="background-image">
+						Logo, Links, UI, etc.
+						<div className="container valign-wrapper splash-container">
+							<div className="row">
+								<div className="col s12 center-align">
+									<p className="logo-text">Re:invent</p>
+								</div>
+								
+									<div className="col s12 center-align links-container">
+										<a className="auth-links button" href="/register"
+												style={{
+													width: "150px",
+													borderRadius: "3px",
+													letterSpacing: "1.5px"
+												}}
+											>
+												Register
+											</a>
 
-					{/* Logo, Links, UI, etc. */}
-					<div className="container valign-wrapper splash-container">
-						<div className="row">
-							<div className="col s12 center-align">
-								<p className="logo-text">Re:invent</p>
+											<a className="auth-links button" href="/login"
+												style={{
+													marginLeft: "5rem",
+													width: "150px",
+													borderRadius: "3px",
+													letterSpacing: "1.5px"
+												}}
+											>
+												Log In
+											</a>
+									</div>
+						
 							</div>
 							
-								<div className="col s12 center-align links-container">
-									<a className="auth-links button" href="/register"
-											style={{
-												width: "150px",
-												borderRadius: "3px",
-												letterSpacing: "1.5px"
-											}}
-										>
-											Register
-										</a>
-
-										<a className="auth-links button" href="/login"
-											style={{
-												marginLeft: "5rem",
-												width: "150px",
-												borderRadius: "3px",
-												letterSpacing: "1.5px"
-											}}
-										>
-											Log In
-										</a>
-								</div>
-					
 						</div>
-						
+
+									
+
+					
+					</div>
+					<div className="ripple-background">
+						<div className="circle xxlarge shade1"></div>
+						<div className="circle xlarge shade2"></div>
+						<div className="circle large shade3"></div>
+						<div className="circle medium shade4"></div>
+						<div className="circle small shade5"></div>
 					</div>
 
-								
-
-				
+					
 				</div>
-				<div className="ripple-background">
-					<div className="circle xxlarge shade1"></div>
-					<div className="circle xlarge shade2"></div>
-					<div className="circle large shade3"></div>
-					<div className="circle medium shade4"></div>
-					<div className="circle small shade5"></div>
-				</div>
-
-				
-			</div>
-
+			</Parallax>
 			{/* About Page */}
 			<div className="about-container">
 				<div className="container">
