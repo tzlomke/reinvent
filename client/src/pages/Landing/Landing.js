c5af4caed7f9fbc02a5f9407afe0664bbb459dea
Taylor Zlomke <taylor.zlomke@gmail.com>
2019-04-01 00:58:36 -0600

    autoscroll functioning, needs to be slower

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,31 +1,50 @@
-import React from "react";
-import SpaceGirl from "../../images/Retro-Astronaut2.png"
+import React, { Component } from "react";
+import SpaceGirl from "../../images/Retro-Astronaut2.png";
+import ReactDOM from "react-dom";
 import "./style.css";
 import { Parallax } from "react-parallax";
 
 
-const Landing = (props) => {
+class Landing extends Component {
 	
-	return (
-		
-		<div className="landing-page-container">
-			<Parallax
-				bgImage={SpaceGirl}
-				bgImageAlt="Girl in Space Helmet"
-				strength={300}
-			>
-				{/* Splash Page */}
-				<div className="background-container">
-					{/* Animated Background */}
-					<div>
+	constructor(props) {
+		super(props);
+		this.state = { width: 0, height: 0 };
+		this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+	}
+	  
+	componentDidMount() {
+		this.updateWindowDimensions();
+		window.addEventListener('resize', this.updateWindowDimensions);
+	}
+	  
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.updateWindowDimensions);
+	}
+	  
+	updateWindowDimensions() {
+		this.setState({ width: window.innerWidth, height: window.innerHeight });
+	}
 
-						Logo, Links, UI, etc.
-						<div className="container valign-wrapper splash-container">
-							<div className="row">
-								<div className="col s12 center-align">
-									<p className="logo-text">Re:invent</p>
-								</div>
-								
+	render (props) {
+		return (
+			
+			<div className="landing-page-container">
+				<Parallax
+					bgImage={SpaceGirl}
+					bgImageAlt="Girl in Space Helmet"
+					strength={310}
+				>
+					{/* Splash Page */}
+					<div className="background-container">
+						{/* Animated Background */}
+						<div>
+							<div className="container valign-wrapper splash-container">
+								<div className="row">
+									<div className="col s12 center-align">
+										<p className="logo-text">Re:invent</p>
+									</div>
+									
 									<div className="col s12 center-align links-container">
 										<a className="auth-links button" href="/register"
 												style={{
@@ -48,61 +67,68 @@ const Landing = (props) => {
 												Log In
 											</a>
 									</div>
-						
-							</div>
-							
-						</div>
-
 									
-
-					
-					</div>
-					<div className="ripple-background">
-						<div className="circle xxlarge shade1"></div>
-						<div className="circle xlarge shade2"></div>
-						<div className="circle large shade3"></div>
-						<div className="circle medium shade4"></div>
-						<div className="circle small shade5"></div>
-					</div>
-
-					
-				</div>
-			</Parallax>
-			{/* About Page */}
-			<div className="about-container">
-				<div className="container">
-					
-					{/* Display Font */}
-					<div className="row">
-						<div className="col s12 display-text">
-							
-								<div>
-									<div className="center">
-										<h1>
-											What's the big idea?!
-										</h1>
+									<div className="row">
+										<div className="col s12 center-align">
+											<a href="#" onClick={() => {window.scroll({top: this.state.height, left: 0, behavior: 'smooth' })}}>&#10549;</a>
+										</div>
 									</div>
 								</div>
-							
-						</div>
-					</div>
-					
-					{/* Text Content */}
-					<div className="row">
-						<p className="text-content">
-							Hello
-						</p>
-					</div>
+								
+							</div>
 
+										
+
+						
+						</div>
+						<div className="ripple-background">
+							<div className="circle xxlarge shade1"></div>
+							<div className="circle xlarge shade2"></div>
+							<div className="circle large shade3"></div>
+							<div className="circle medium shade4"></div>
+							<div className="circle small shade5"></div>
+						</div>
+
+						
+					</div>
+				</Parallax>
+
+
+				{/* About Page */}
+				<div className="about-container" ref={this.myRef}>
+					<div className="container">
+						
+						{/* Display Font */}
+						<div className="row">
+							<div className="col s12 display-text">
+								
+									<div>
+										<div className="center">
+											<h1>
+												What's the big idea?!
+											</h1>
+										</div>
+									</div>
+								
+							</div>
+						</div>
+						
+						{/* Text Content */}
+						<div className="row">
+							<p className="text-content">
+								Hello
+							</p>
+						</div>
+
+					</div>
 				</div>
+
 			</div>
 
-		</div>
-
-		
-		
-	);
-
+			
+			
+		);
+	}
 }
 
 
