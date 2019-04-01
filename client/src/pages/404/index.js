a5611710bf9529221528d1979132238b551f4789
Akop K <akop87@gmail.com>
2019-04-01 16:50:30 -0600

    added title to 404 page to match the rest of the application styling.

--- client/src/pages/404/index.js
+++ client/src/pages/404/index.js
@@ -3,13 +3,17 @@ import donotpass from '../../images/none_shall_pass.jpg';
 import './style.css';
 import Header from '../../components/Header';
 import { Col, Row, Container } from "../../components/Grid";
+import { Title, SubTitle } from "../../components/Title";
+
 
 const page404 = () => {
   return(
     <div>
       <Header />
       <Container>
-        {/* <h1>404 Error</h1> */}
+        <Title
+          titleText="Page not Found"
+        />
         <div id="container404">
         <a id="return_link" href="/dashboard">Take Me Home</a>
           <img id="dead_link" src={donotpass} alt="Dead Link"></img>
