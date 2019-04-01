aafdcbcad4c1690a84435bb5ab5d7dc723b44e47
Taylor Zlomke <taylor.zlomke@gmail.com>
2019-03-31 20:43:30 -0600

    styled splash page

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -3,59 +3,73 @@ import lightbulbCutout from "../../images/lightbulbCutout.png";
 import "./style.css";
 
 
+
 const Landing = (props) => {
 	
 	return (
 		
-		<div className="landingPageContainer">
-			 
-			<div className="container logoContainer">
-				<div class="row">
-					<div class="col s12">
-						<img className="logoImage" src={lightbulbCutout} alt="Light Bulb"></img>
-						<div className="logoTextContainer center-align">
-							<h1 className="logoHatch center-align">Re:invent</h1>
-							{/* <h1 className="logoOutline center-align">Re:invent</h1> */}
+		<div className="landing-page-container">
+
+			{/* Splash Page */}
+			<div className="background-container">
+				{/* Animated Background */}
+				<div className="background-image">
+
+					{/* Logo, Links, UI, etc. */}
+					<div className="container valign-wrapper splash-container">
+						<div className="row">
+							<div className="col s12 center-align">
+								<h1 className="logo-text">Re:invent</h1>
+							</div>
+							
+								<div className="col s12 center-align links-container">
+									<a className="auth-links button" href="/register"
+											style={{
+												width: "150px",
+												borderRadius: "3px",
+												letterSpacing: "1.5px"
+											}}
+										>
+											Register
+										</a>
+
+										<a className="auth-links button" href="/login"
+											style={{
+												marginLeft: "5rem",
+												width: "150px",
+												borderRadius: "3px",
+												letterSpacing: "1.5px"
+											}}
+										>
+											Log In
+										</a>
+								</div>
+					
 						</div>
+						
 					</div>
+
+								
+
+				
 				</div>
-			</div>
-			
-			<br />
-			
-			<div style={{ height: "75vh" }} className="container valign-wrapper">
-				<div className="row">
-					<div className="col s12 center-align">
-						<br />
-
-						<a href="/register"
-							style={{
-								width: "150px",
-								borderRadius: "3px",
-								letterSpacing: "1.5px"
-							}}
-							className="tomato"
-						>
-							Register
-						</a>
-
-						<a href="/login"
-							style={{
-								marginLeft: "5rem",
-								width: "150px",
-								borderRadius: "3px",
-								letterSpacing: "1.5px"
-							}}
-							className="tomato"
-						>
-							Log In
-						</a>
-
-					</div>
+				<div className="ripple-background">
+					<div className="circle xxlarge shade1"></div>
+					<div className="circle xlarge shade2"></div>
+					<div className="circle large shade3"></div>
+					<div className="circle medium shade4"></div>
+					<div className="circle small shade5"></div>
 				</div>
+
+				
 			</div>
+
+			
 
 		</div>
+
+		
+		
 	);
 
 }
