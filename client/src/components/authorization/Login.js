9b9453156f37dc3b92c0b217525ee07f54452e27
Taylor Zlomke <taylor.zlomke@gmail.com>
2019-04-02 23:22:08 -0600

    if user is logged in and navigates to login/register, redirect to news feed

--- client/src/components/authorization/Login.js
+++ client/src/components/authorization/Login.js
@@ -17,15 +17,15 @@ class Login extends Component {
 	};
 
 	componentDidMount() {
-		// If logged in and user navigates to Login page, should redirect them to dashboard
+		// If logged in and user navigates to Login page, should redirect them to news-feed
 		if (this.props.auth.isAuthenticated) {
-		  	this.props.history.push("/dashboard");
+		  	this.props.history.push("/news-feed");
 		}
 	};
 
 	componentWillReceiveProps(nextProps) {
 		if (nextProps.auth.isAuthenticated) {
-		  	this.props.history.push("/dashboard"); // push user to dashboard when they login
+		  	this.props.history.push("/news-feed"); // push user to news feed when they login
 		}
 		
 		if (nextProps.errors) {
